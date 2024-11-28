ebfad9ff22d0d35b4cb2e8cdbeb7401a81f48392
Irina Samoylova <novikova.i.v@gmail.com>
2024-11-28 15:56:53 +0300

    создана функция для получения изображения из запроса

--- cats.py
+++ cats.py
@@ -4,8 +4,17 @@ import requests
 
 from io import BytesIO#ввод вывод информации в двоичном виде
 def load_image(url):
+    try:
+        response = requests.get(url)
+        # если возникнет ошибка при отправке запроса то проверим это
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f'Error {e}.')
+        return None
 
-    pass
 #графический интерфейс для отправки запросов к api
 window = Tk()
 window.title('Cats')
