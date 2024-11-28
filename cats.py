08adb86441a2e90ac19605b7e47e347829d5d1c6
Irina Samoylova <novikova.i.v@gmail.com>
2024-11-28 15:50:58 +0300

    создали графический интерфейс и функция загрузки изображения в виде заглушки

--- cats.py
+++ cats.py
@@ -1,1 +1,22 @@
 from tkinter import *
+from PIL import Image, ImageTk # библиотеки для загрузки изображения
+import requests
+
+from io import BytesIO#ввод вывод информации в двоичном виде
+def load_image(url):
+
+    pass
+#графический интерфейс для отправки запросов к api
+window = Tk()
+window.title('Cats')
+window.geometry('600x400')
+label = Label()
+label.pack()
+
+url = 'htts://cataas.com/cat'
+img = load_image(url)
+if img:
+    label.config(image=img)
+    label.image = img
+    #если картинка присвоена, то сборщик мусора ее не удалит
+window.mainloop()
